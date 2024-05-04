82ec1ec1dd0d1775f3e5615fa5f837c78c4f5f32
Erkan Gazi <erkangazi20@gmail.com>
2024-05-04 17:55:00 +0300

    Completed excercise

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function calculateBill {
-    
+function calculateBill (price, vat, tip) {
+    let totalPrice = sum = price + vat + tip;
+    if (typeof price !== 'number') {
+        return "error";
+    } else {
+    if (typeof vat !== 'number') {
+        return "error";
+    } else {
+        if (typeof tip !== 'number') {
+            return "error";
+        } else {
+    return "£" + totalPrice;
+        }
+    }
+}
 }
 
 
